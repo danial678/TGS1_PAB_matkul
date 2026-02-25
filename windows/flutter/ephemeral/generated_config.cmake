245370b3e4656e2f2276b6966d9f65f39403a839
danial678 <dhirzanakbary@gmail.com>
2026-02-25 21:54:37 +0800

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\MSI GK\\Downloads\\flutter_windows_3.38.9-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Danial\\KULIAH TGs\\pak anton\\tgs1_PAB" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\MSI GK\\Downloads\\flutter_windows_3.38.9-stable\\flutter"
+  "PROJECT_DIR=C:\\Danial\\KULIAH TGs\\pak anton\\tgs1_PAB"
+  "FLUTTER_ROOT=C:\\Users\\MSI GK\\Downloads\\flutter_windows_3.38.9-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Danial\\KULIAH TGs\\pak anton\\tgs1_PAB\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Danial\\KULIAH TGs\\pak anton\\tgs1_PAB"
+  "FLUTTER_TARGET=C:\\Danial\\KULIAH TGs\\pak anton\\tgs1_PAB\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzguOQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NjczMjNkZTI4NQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NTg3YzE4Zjg3Mw==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC44"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Danial\\KULIAH TGs\\pak anton\\tgs1_PAB\\.dart_tool\\package_config.json"
+)
